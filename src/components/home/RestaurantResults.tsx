eca2986a8fe16fdd8f352d974f098a67df689c0d
oobaretin <your-email@example.com>
2026-01-27 23:14:18 -0600

    fix: remove unused Link import from RestaurantResults

--- src/components/home/RestaurantResults.tsx
+++ src/components/home/RestaurantResults.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import * as React from "react";
-import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { RestaurantCardWithDistance } from "./RestaurantCardWithDistance";
 import type { JSONRestaurant } from "@/lib/restaurant-json-loader";
